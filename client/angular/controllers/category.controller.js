ff2cf0c14742802525da84cfb164203d25d99e48
Shubham Karmakar <sk@workspace.com>
2017-12-24 22:35:24 +0530

    removed timeout from category controller

--- client/angular/controllers/category.controller.js
+++ client/angular/controllers/category.controller.js
@@ -5,65 +5,65 @@
 quizApp.controller("category", ['$scope', '$http', '$window', 'authFactory', 'localStorageService', '$uibModal', '$log', '$timeout',
     function($scope, $http, $window, authFactory, localStorageService, $uibModal, $log, $timeout) {
         var _id = authFactory._id;
-        $timeout(function() {
-            if (_id === null) {
+
+        if (_id === null) {
+            $window.location.href = "#/home";
+        } else {
+
+            $window.scrollTo(0, 0);
+            $scope.user = {
+                name: authFactory.fname
+            };
+            $scope.logout = function() {
+
+                localStorageService.clearAll();
+                $window.location.reload();
                 $window.location.href = "#/home";
             }
-        }, 100);
+            $scope.animationsEnabled = false;
 
-        $window.scrollTo(0, 0);
-        $scope.user = {
-            name: authFactory.fname
-        };
-        $scope.logout = function() {
+            $scope.openTestInfo = function(size, category) {
 
-            localStorageService.clearAll();
-            $window.location.reload();
-            $window.location.href = "#/home";
-        }
-        $scope.animationsEnabled = false;
+                var modalInstance = $uibModal.open({
+                    animation: $scope.animationsEnabled,
+                    templateUrl: '/angular/views/test-info.html',
+                    controller: function($scope, $uibModalInstance, category) {
+                        $scope.category = category;
+                        $scope.ok = function() {
+                            $uibModalInstance.close();
+                        };
 
-        $scope.openTestInfo = function(size, category) {
-
-            var modalInstance = $uibModal.open({
-                animation: $scope.animationsEnabled,
-                templateUrl: '/angular/views/test-info.html',
-                controller: function($scope, $uibModalInstance, category) {
-                    $scope.category = category;
-                    $scope.ok = function() {
-                        $uibModalInstance.close();
-                    };
-
-                    $scope.cancel = function() {
-                        $uibModalInstance.dismiss('cancel');
-                    };
-                },
-                size: size,
-                resolve: {
-                    category: function() {
-                        return category;
+                        $scope.cancel = function() {
+                            $uibModalInstance.dismiss('cancel');
+                        };
+                    },
+                    size: size,
+                    resolve: {
+                        category: function() {
+                            return category;
+                        }
                     }
-                }
-            });
-
-            modalInstance.result.then(function(selectedItem) {
-                $scope.selected = selectedItem;
-            }, function() {})
-        };
-        var main = this;
-        main.leaderBoard = [];
-        main.response = {};
-        $http.get('/api/v1/users')
-            .then(function(response) {
-
-                angular.forEach(response.data.data, function(user) {
-                    main.leaderBoard.push({ name: user.fname, points: user.totalPoints });
                 });
 
-            })
-            .catch((err) => {
-                alert('Some error occurred while retrieving leaderBoard');
-            })
+                modalInstance.result.then(function(selectedItem) {
+                    $scope.selected = selectedItem;
+                }, function() {})
+            };
+            var main = this;
+            main.leaderBoard = [];
+            main.response = {};
+            $http.get('/api/v1/users')
+                .then(function(response) {
 
+                    angular.forEach(response.data.data, function(user) {
+                        main.leaderBoard.push({ name: user.fname, points: user.totalPoints });
+                    });
+
+                })
+                .catch((err) => {
+                    alert('Some error occurred while retrieving leaderBoard');
+                })
+
+        }
     }
 ]);
