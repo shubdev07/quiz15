d54be4c440aef6c1b10a5a0087326b24e7c6d96a
Shubham Karmakar <sk@workspace.com>
2017-12-25 22:33:21 +0530

    testing

--- client/angular/controllers/category.controller.js
+++ client/angular/controllers/category.controller.js
@@ -5,10 +5,13 @@
 quizApp.controller("category", ['$scope', '$http', '$window', 'authFactory', 'localStorageService', '$uibModal', '$log', '$timeout',
     function($scope, $http, $window, authFactory, localStorageService, $uibModal, $log, $timeout) {
         var _id = authFactory._id;
+        alert(_id);
         $timeout(() => {
             if (_id === null) {
+                alert(_id);
                 $window.location.href = "#/home";
             } else {
+                alert('else wala ' + _id);
 
                 $window.scrollTo(0, 0);
                 $scope.user = {
